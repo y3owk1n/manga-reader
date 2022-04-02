5511d85c81e1c0fc0fe28e7b342247c846c26572
y3owk1n <wongyeowkin@gmail.com>
2022-04-02 15:04:33 +0800

    reupdate weserv

--- utils/replaceToMuwai.ts
+++ utils/replaceToMuwai.ts
@@ -1,7 +1,7 @@
 export const replaceToMuwai = (url: string) => {
   const toHttp = url.replace(/^https?:\/\//, "");
-  const toMuwai = toHttp.replace("dmzj", "muwai");
-  const toWeserv = `//images.weserv.nl?url=${toMuwai}&output=webp`;
+  const toMuwai = url.replace("dmzj", "muwai");
+  const toWeserv = `https://images.weserv.nl?url=${toMuwai}&output=webp`;
 
   return toWeserv;
 };
