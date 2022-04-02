83a0b7c819a769cae6adcd0d529546f8157d8870
y3owk1n <wongyeowkin@gmail.com>
2022-04-02 15:12:09 +0800

    updated to https images v2

--- utils/replaceToMuwai.ts
+++ utils/replaceToMuwai.ts
@@ -1,5 +1,5 @@
 export const replaceToMuwai = (url: string) => {
-  const toHttps = url.replace(/^http?:\/\//, "https");
+  const toHttps = url.replace(/^http?:\/\//, "https://");
   const toMuwai = toHttps.replace("dmzj", "muwai");
   const toWeserv = `https://images.weserv.nl?url=${toMuwai}&output=webp`;
 
